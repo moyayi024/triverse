e1de2682fe2ab51dc40e31514786f561e5343150
Joshua "GrahamLord" Moya <i018@ilmadlocalsvr.com>
2019-04-29 13:54:53 +0800

    import project

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+      <!-- Triverse -->
+      <div class="row">
+        <div class="col-sm-8">
+             <div style="padding:80px;">
+                <div class="row" >
+                <h1>Welcome to Triverse</h1>
+                <div style="color:white; padding-top:10px; font-size:20px;"> 
+                  <p><b>Triverse is an app that can help you and your community to have easy access for information, news and update in your area.</b></p>
+                </div>
+                <div class="col-xs-4">
+                  <div>
+                    <img class="responsive" alt="Vue logo" style="text-align:center; margin-bottom:10px;" src="./assets/web.png" width="260" height="218">
+                  </div>
+                </div>
+                <div class="col-xs-4">
+                  <div style="padding-top:80px;padding-left:10px;">
+                    <img class="responsive" alt="Vue logo" style="text-align:center; margin-bottom:10px;" src="./assets/mobile.png" width="86" height="132">      
+                  </div>
+                </div>  
+             </div>
+           </div>
+        </div>
+        <!-- Login -->
+        <div class="col-sm-4"  style="background-color:#F5F5F5; padding:0px; height:100vh;">
+          <div class="text-center" style="padding:50px">
+           <div class="text-center">
+             <img class="responsive" alt="Vue logo" style="text-align:center; margin-bottom:10px;" src="./assets/logotriverse.png" width="250" height="250">
+          </div>
+          <div class="login-dark">
+            <form method="post">
+              <h2 class="sr-only">Login Form</h2>
+              <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
+              <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+              <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+              <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Log In</button></div><a href="#" class="forgot">Forgot your email or password?</a>
+            </form>
+          </div>
+          </div>  
+         
+          <div style="background-color:#42f4a1;width:100%; position:absolute; bottom: 0;">
+              <div class="text-center" style="color:white; padding-top:10px;"> <p>By continuing you accept our <b>Terms of Use</b></p></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'app',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  
+}
+</style>
---
+++ src/Dashboard.vue
@@ -0,0 +1,59 @@
+<template>
+<div id="containerpills" class="container">
+  <h2>Welcome Brgy Tibay!</h2>
+  <br>
+  <!-- Nav pills -->
+  <ul class="nav nav-pills" role="tablist">
+    <li class="nav-item">
+      <a class="nav-link active" data-toggle="pill" href="#home">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="pill" href="#menu1">Reservation</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="pill" href="#menu2">Reports</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="pill" href="#menu3">Records</a>
+    </li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div id="home" class="container tab-pane active"><br>
+      <h3>Timeline</h3>
+      <p>dito sa page nato ilalagay yung mga announcements bale mauuna yung pinakahule</p>
+    </div>
+    <div id="menu1" class="container tab-pane fade"><br>
+      <h3>Reservation</h3>
+      <p>court reservation/tent/events(zumba)</p>
+    </div>
+    <div id="menu2" class="container tab-pane fade"><br>
+      <h3>Reports</h3>
+      <p>dito ilalagay yung mga incidente ng nakawan blotter na parang bitag</p>
+    </div>
+    <div id="menu3" class="container tab-pane fade"><br>
+      <h3>Records</h3>
+      <p>dito yung mga magrerquest ng mga papers/ brgy id / brgy clearance etc.</p>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#containerpills {
+  position: relative;
+  height: 90%;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px 20px 30px 20px;
+  margin-top: 30px;
+}
+</style>
